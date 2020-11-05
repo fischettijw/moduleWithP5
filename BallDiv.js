7c14d73a6e3837d9f8cc3a95d0726ff5a4463f39
Joseph Fischetti <fischettijw@gmail.com>
2020-11-05 08:06:48 -0500

    i think it's ALL working

--- BallDiv.js
+++ BallDiv.js
@@ -1,12 +1,12 @@
 import Ball from "./Ball.js";
 export default class BallDiv {
-    constructor(canvasWidth, canvasHeigh, colorBG, ballSize, colorBall, div) {
+    constructor(canvasWidth, canvasHeigh, colorBG, ballSize, colorBall, xVel, yVel, div) {
         let ball;
 
         new p5(function(p) {
             p.setup = function() {
                 p.createCanvas(canvasWidth, canvasHeigh);
-                ball = new Ball(p.width / 2, p.height / 2, ballSize, colorBall, -3, 2);
+                ball = new Ball(p.width / 2, p.height / 2, ballSize, colorBall, xVel, yVel);
             }
 
             p.draw = function() {
