01bc1a38b20a3aca455ffe5305174682ab92db8f
Joseph Fischetti <fischettijw@gmail.com>
2020-11-06 05:31:33 -0500

    multiple balls

--- BallDiv.js
+++ BallDiv.js
@@ -1,18 +1,25 @@
 import Ball from "./Ball.js";
 export default class BallDiv {
     constructor(canvasWidth, canvasHeigh, colorBG, ballSize, colorBall, xVel, yVel, div) {
-        let ball;
+        let ball = [];
+        let nb = 10;
 
         new p5(function(p) {
             p.setup = function() {
                 p.createCanvas(canvasWidth, canvasHeigh);
-                ball = new Ball(p.width / 2, p.height / 2, ballSize, colorBall, xVel, yVel);
+                for (let i = 0; i < nb; i++) {
+                    let rx = p.random(0, 10);
+                    let ry = Math.random(0, 10);
+                    ball[i] = new Ball(p.width / 2, p.height / 2, ballSize, colorBall, xVel + rx, yVel + ry);
+                }
             }
 
             p.draw = function() {
                 p.background(colorBG);
-                ball.show(p);
-                ball.move(p);
+                for (let i = 0; i < nb; i++) {
+                    ball[i].show(p);
+                    ball[i].move(p);
+                }
             }
         }, div);
     }
