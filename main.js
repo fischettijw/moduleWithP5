e669b9aa0a9a0a08e9026ed45d7d8b9379d21425
Joseph Fischetti <fischettijw@gmail.com>
2020-11-07 07:17:30 -0500

    before Miguel Lesson

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import BallDiv from "./BallDiv.js";
-
 new BallDiv(400, 200, 'yellow', 30, 'red', 1, 2, 'yellow');
 new BallDiv(450, 200, 'green', 40, 'yellow', -2, 3, 'green');
 new BallDiv(300, 200, 'gray', 20, 'magenta', 2, -1, 'gray');
